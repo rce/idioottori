dbef1bacb2baee88da8005fc5201471d6e3de4c3
Henry Heikkinen <rce@rce.fi>
2019-09-09 18:32:08 +0300

    Update dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const CleanWebpackPlugin = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const merge = require("webpack-merge")
 
 const srcDir = path.resolve(__dirname, "src")
